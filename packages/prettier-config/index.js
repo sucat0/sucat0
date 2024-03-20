afd3aa14afbc3818c1bdaee19435545a27dd5828
sucat1007 <sucat1007@gmail.com>
2024-03-20 18:10:22 +0900

    feat(prettier-config): add sort-import plugin to config

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,7 +1,10 @@
-module.exports = {
+const config = {
     "printWidth": 120,
     "tabWidth": 4,
     "trailingComma": "all",
     "singleQuote": true,
-    "semi": false
+    "semi": false,
+    "plugins": ["@trivago/prettier-plugin-sort-imports"]
 }
+
+export default config;
